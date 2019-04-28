785a40eba55264b8b5d1f41109109df4adcf7aed
Darcy <github@darcythomas.com>
2019-04-28 22:18:33 +1200

    basic

---
+++ ClassLibrary1/Ping.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Ping : IRequest<string> {
+        [FromRoute (Name ="id")]
+        public int Id { get; set; }
+    }
+
+   
+    public partial class QueryValidator : AbstractValidator<Ping>
+    {
+        public QueryValidator()
+        {
+            RuleFor(m => m.Id).NotNull();
+           // RuleFor(m => m.Id).GreaterThan(2);
+        }
+    }
+
+    public class PingService : IPingService {
+
+        public Task<string> Query(Ping query)
+        {
+            return Task.FromResult($"Pong {query.Id}");
+        }
+
+    }
+
+    public interface IPingService
+    {
+        Task<string> Query(Ping query);
+    }
+}
---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLibrary1;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        private readonly IMediator _mediator;
+
+        public ValuesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> GetAsync(Ping ping)
+        {
+            if (!ModelState.IsValid)
+            {
+                return "nope";
+            }
+            var result = await _mediator.Send(ping);
+            return result;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
